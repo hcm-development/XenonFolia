9d763e8c3ec52d204dbc758ed58d8eab7995fdb8
khoasoma <songtu438@gmail.com>
2025-08-08 10:33:23 +0700

    Initial commit: Add core project files

---
+++ README.md
@@ -0,0 +1,85 @@
+<img src="./public/image/LightingLuminol_LL方_白_字.png" alt="Logo" align="right" width="250">
+
+# XenonFolia
+
+<h4>XenonFolia 是一个基于Luminol的分支，具有许多有用的优化、可配置的原版特性和更多的API支持，目标是在Folia上运行更多Bukkit插件</h4>
+
+[![Issues](https://img.shields.io/github/issues/XenonMC/XenonFolia?style=flat-square)](https://github.com/XenonMC/XenonFolia/issues)
+![Commit Activity](https://img.shields.io/github/commit-activity/w/XenonMC/XenonFolia?style=flat-square)
+![GitHub all releases](https://img.shields.io/github/downloads/XenonMC/XenonFolia/total?style=flat-square)
+
+[English](./README_EN.md) | **中文**
+
+## 特性
+ - 支持部分Bukkit插件
+ - 可配置的原版特性
+ - Tpsbar 支持
+ - 对单线程区域性能的优化
+ - 更多的插件开发API支持（进行中）
+
+## 下载
+任何版本都可以在 [Release](https://github.com/XenonMC/XenonFolia/releases), 中找到，你也可以通过[以下步骤](./README.md#构建)自己构建。
+
+## 构建
+要构建一个paperclip jar，你需要运行以下命令。你可以在xenonfolia-server/build/libs中找到jar（注意：需要`JDK21`）
+ ```shell
+./gradlew applyAllPatches && ./gradlew createMojmapPaperclipJar
+```
+## 使用API
+使用 Gradle:
+
+```kotlin
+repositories {
+    maven {
+        url = "https://repo.menthamc.com/repository/maven-public/"
+    }
+}
+
+dependencies {
+    compileOnly("me.earthme.xenonfolia:xenonfolia-api:$VERSION")
+}
+ ```
+
+使用 Maven
+
+```xml
+<repositories>
+    <repository>
+        <id>menthamc</id>
+        <url>https://repo.menthamc.com/repository/maven-public/</url>
+    </repository>
+</repositories>
+
+<dependencies>
+    <dependency>
+        <groupId>me.earthme.xenonfolia</groupId>
+        <artifactId>luminol-api</artifactId>
+        <version>$VERSION</version>
+    </dependency>
+</dependencies>
+```
+
+## 联系方式
+> 如果您对这个项目感兴趣或有任何问题，请随时向我们提问。
+
+**QQ群: [1015048616](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=QML5kIVsniPi1PlZvnjHQT_02EHsZ5Jc&authKey=%2FTCJsZC7JFQ9sxAroPCKuYnlV57Z5fyqp36ewXZk3Sn4iJ9p4MB1JKdc%2FFcX3HOM&noverify=0&group_code=1015048616)** | QQ频道: [点击加入](https://pd.qq.com/s/eq9krf9j) | Telegram: [点击加入](https://t.me/LuminolMinecraft) | Discord: [点击加入](https://discord.gg/Qd7m3V6eDx)
+
+## 关于 Issue
+当您遇到任何问题时，请向我们提问，我们将尽力解决，但请记得清楚地描述您的问题并提供足够的日志等信息。
+
+## 贡献代码
+可[查看 Contributing](./docs/CONTRIBUTING.md)
+
+## BStats
+![bStats](https://bstats.org/signatures/server-implementation/Luminol.svg "bStats")
+
+## 请给我们一个 ⭐Star！
+> 你的每一个免费的 ⭐Star 就是我们每一个前进的动力。
+
+<a href="https://star-history.com/#LuminolMC/Luminol&LuminolMC/LightingLuminol&Date">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://api.star-history.com/svg?repos=LuminolMC/Luminol%2CLuminolMC/LightingLuminol&type=Date&theme=dark" />
+    <source media="(prefers-color-scheme: light)" srcset="https://api.star-history.com/svg?repos=LuminolMC/Luminol%2CLuminolMC/LightingLuminol&type=Date" />
+    <img alt="Star历史表" src="https://api.star-history.com/svg?repos=LuminolMC/Luminol%2CLuminolMC/LightingLuminol&type=Date" />
+  </picture>
+</a>
---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java // TODO java launcher tasks
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.17"
+}
+
+paperweight {
+    upstreams.register("luminol") {
+        repo = github("LuminolMC", "Luminol")
+        ref = providers.gradleProperty("luminolRef")
+
+        patchFile {
+            path = "luminol-server/build.gradle.kts"
+            outputFile = file("xenonfolia-server/build.gradle.kts")
+            patchFile = file("xenonfolia-server/build.gradle.kts.patch")
+        }
+        patchFile {
+            path = "luminol-api/build.gradle.kts"
+            outputFile = file("xenonfolia-api/build.gradle.kts")
+            patchFile = file("xenonfolia-api/build.gradle.kts.patch")
+        }
+        patchRepo("paperApi") {
+            upstreamPath = "paper-api"
+            patchesDir = file("xenonfolia-api/paper-patches")
+            outputDir = file("paper-api")
+        }
+        patchRepo("foliaApi") {
+            upstreamPath = "folia-api"
+            patchesDir = file("xenonfolia-api/folia-patches")
+            outputDir = file("folia-api")
+        }
+        patchDir("luminolApi") {
+            upstreamPath = "luminol-api"
+            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
+            patchesDir = file("xenonfolia-api/luminol-patches")
+            outputDir = file("luminol-api")
+        }
+    }
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
+
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 21
+        options.isFork = true
+        options.forkOptions.memoryMaximumSize = "6g" // Prevent OOM during building
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
+        }
+    }
+/*
+    extensions.configure<PublishingExtension> {
+        repositories {
+            maven("https://repo.menthamc.com/repository/maven-snapshots/") {
+                name = "MenthaMC"
+                credentials(PasswordCredentials::class) {
+                    username = System.getenv("MAVEN_REPO_USER")
+                    password = System.getenv("MAVEN_REPO_PASSWORD")
+                }
+            }
+        }
+    }
+*/
+     tasks.withType<Javadoc> {
+        options {
+            (this as StandardJavadocDocletOptions).apply {
+                addStringOption("-add-modules", "jdk.incubator.vector")
+                addStringOption("Xdoclint:none", "-quiet")
+            }
+        }
+    }
+}
